a4051c6e5822a04b880402c015b3c3d4da1e3147
Lada <roskova.l@yandex.ru>
2023-10-09 19:17:52 +0300

    initial commit

---
+++ studybud/base/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/<str:pk>', views.room, name='room'),
+    path('create-room', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+    path('profile/<str:pk>/', views.user_profile, name='user-profile'),
+    path('update-user/', views.update_user, name='update-user'),
+    path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
+]
